83874c02749e78c206c0a364f128c0cbdcaa9744
Ilia <iliaking97@gmail.com>
2017-03-05 20:37:26 +0200

    Prim algorithm has been added.

--- Lab1/MinimumFrame/MinimumFrame/Program.cs
+++ Lab1/MinimumFrame/MinimumFrame/Program.cs
@@ -8,50 +8,14 @@ namespace MinimumFrame
     {
         static void Main(string[] args)
         {
-            string[] firstStr = Console.ReadLine().Split(' ');
-
-            var verticesCount = Convert.ToInt32(firstStr[0]);
-            var edgesCount = Convert.ToInt32(firstStr[1]);
-
-            int[,] graph = new int[verticesCount, verticesCount];
-            for(int i = 0; i < graph.GetLength(0); i++)
-            {
-                for (int j = 0; j < graph.GetLength(1); j++)
-                {
-                    graph[i, j] = int.MaxValue;
-                }
-            }
-            List<Edge> edges = new List<Edge>();
-
-            for (int i = 0; i < edgesCount; i++)
-            {
-                var nextStr = Console.ReadLine().Split(' ');
-
-                var firstVertexNumber = Convert.ToInt32(nextStr[0]) - 1;
-                var secondVertexNumber = Convert.ToInt32(nextStr[1]) - 1;
-                int edgeWeight = Convert.ToInt32(nextStr[2]);
-
-                edges.Add(new Edge()
-                {
-                    NewVetrexNumber = firstVertexNumber,
-                    SelectedVetrexNumber = secondVertexNumber,
-                    Weight = edgeWeight
-                });
-
-                if (graph[firstVertexNumber, secondVertexNumber] == 0 || graph[firstVertexNumber, secondVertexNumber] > edgeWeight)
-                {
-                    graph[firstVertexNumber, secondVertexNumber] = edgeWeight;
-                    graph[secondVertexNumber, firstVertexNumber] = edgeWeight;
-                }
-            }
-
-            Console.WriteLine(PrimeAlhorithmOptimized(graph));
+            PrimAlhorithmOptimized();
             // Console.WriteLine(PrimeAlhorithm(graph));
-            // Console.WriteLine(KruskallAlhorithm(edges.OrderBy(x => x.Weight).ToList()));
+            // Console.WriteLine(KruskallAlhorithm(edges.OrderBy(x => x.Weight).ToList(), verticesCount));
+            // KruskallAlhorithmOptimized();
         }
 
         // Сложность за n * m
-        static double PrimeAlhorithm(double[,] graph)
+        static double PrimeAlhorithm(int[,] graph)
         {
             var verticesCount = graph.GetLength(0);
             double sum = 0;
@@ -62,7 +26,7 @@ namespace MinimumFrame
             int selectedVerticesCount = 0;
 
             List<Edge> edges = new List<Edge>();
-            
+
             while (selectedVerticesCount < verticesCount)
             {
                 selectedVertices[nextEdgeNumber] = true;
@@ -85,21 +49,21 @@ namespace MinimumFrame
                         edges.Add(edge);
                     }
                 }
-                
+
                 double minEdgeWeight = double.MaxValue;
                 var newNextEdgeNumber = -1;
                 var removeEdgeNumber = -1;
 
                 for (int i = 0; i < edges.Count; i++)
                 {
-                    if(selectedVertices[edges[i].NewVetrexNumber])
+                    if (selectedVertices[edges[i].NewVetrexNumber])
                     {
                         edges.RemoveAt(i);
                         i--;
                         continue;
                     }
 
-                    if(edges[i].Weight < minEdgeWeight)
+                    if (edges[i].Weight < minEdgeWeight)
                     {
                         newNextEdgeNumber = edges[i].NewVetrexNumber;
                         minEdgeWeight = edges[i].Weight;
@@ -117,142 +81,237 @@ namespace MinimumFrame
         }
 
         // Сложность за n квадрат
-        static int PrimeAlhorithmOptimized(int[,] graph)
+        static void PrimAlhorithmOptimized()
         {
-            int sum = 0;
-            int n = graph.GetLength(0);
+            string[] firstStr = Console.ReadLine().Split(' ');
 
-            bool[] used = new bool[n];
-            int[] min_e = new int[n];//INF
-            for(int i = 0; i < min_e.Length; i++)
+            var verticesCount = Convert.ToInt32(firstStr[0]);
+            var edgesCount = Convert.ToInt32(firstStr[1]);
+
+            List<KeyValuePair<int, int>>[] edges = new List<KeyValuePair<int, int>>[verticesCount];
+            for(int i = 0; i < verticesCount; i++)
             {
-                min_e[i] = int.MaxValue;
+                edges[i] = new List<KeyValuePair<int, int>>();
             }
 
-            int[] sel_e = new int[n];//-1
-            for (int i = 0; i < sel_e.Length; i++)
+            for (int i = 0; i < edgesCount; i++)
             {
-                sel_e[i] = -1;
+                var nextStr = Console.ReadLine().Split(' ');
+
+                var firstVertexNumber = Convert.ToInt32(nextStr[0]) - 1;
+                var secondVertexNumber = Convert.ToInt32(nextStr[1]) - 1;
+                int edgeWeight = Convert.ToInt32(nextStr[2]);
+
+                edges[firstVertexNumber].Add(new KeyValuePair<int, int>(secondVertexNumber, edgeWeight));
+                edges[secondVertexNumber].Add(new KeyValuePair<int, int>(firstVertexNumber, edgeWeight));
             }
 
-            min_e[0] = 0;
-            for (int i = 0; i < n; ++i)
+            double sum = 0;
+            int[] minEdges = new int[verticesCount];
+
+            for (int i = 0; i < minEdges.Length; i++)
             {
-                int v = -1;
-                for (int j = 0; j < n; ++j)
-                    if (!used[j] && (v == -1 || min_e[j] < min_e[v]))
-                        v = j;
-                if (min_e[v] == int.MaxValue)
+                minEdges[i] = int.MaxValue;
+            }
+
+            bool[] isUsed = new bool[verticesCount];
+            isUsed[0] = true;
+            
+            SortedSet<Edge> minLengthsOrderedByLength = new SortedSet<Edge>();
+            minLengthsOrderedByLength.Add(new Edge());
+
+            for (int i = 0; i < verticesCount; ++i)
+            {
+                int vertexNumber = minLengthsOrderedByLength.First().NewVetrexNumber;
+                double weight = minLengthsOrderedByLength.First().Weight;
+                minLengthsOrderedByLength.Remove(minLengthsOrderedByLength.First());
+
+                isUsed[vertexNumber] = true;
+                sum += weight;
+
+                for (int j = 0; j < edges[vertexNumber].Count; ++j)
                 {
-                    return -1;
-                }
-
-                used[v] = true;
-                if (sel_e[v] != -1)
-                    sum += graph[v, sel_e[v]];
-
-                for (int to = 0; to < n; ++to)
-                    if (graph[v, to] < min_e[to])
+                    if (!isUsed[edges[vertexNumber][j].Key])
                     {
-                        min_e[to] = graph[v, to];
-                        sel_e[to] = v;
+                        int to = edges[vertexNumber][j].Key;
+                        int cost = edges[vertexNumber][j].Value;
+                        if (cost < minEdges[to])
+                        {
+                            minLengthsOrderedByLength.Remove(new Edge() { NewVetrexNumber = to, Weight = minEdges[to] });
+                            minEdges[to] = cost;
+                            minLengthsOrderedByLength.Add(new Edge() { NewVetrexNumber = to, Weight = minEdges[to] });
+                        }
                     }
+                }
             }
 
-            return sum;
+            Console.WriteLine(sum);
         }
 
         /// <summary>
         /// В данном алгоритме нет разницы между SelectedVetrexNumber и NewVetrexNumber.
         /// </summary>
         /// <param name="edges">Рёбра, отсортированные по возрастанию веса</param>
+        /// <param name="verticesCount">Количество вершин в графе</param>
         /// <returns></returns>
-        static double KruskallAlhorithm(List<Edge> edges)
+        static double KruskallAlhorithm(List<Edge> edges, int verticesCount)
         {
-            List<int> addedVertices = new List<int>();
-            addedVertices.Add(edges[0].NewVetrexNumber);
-            addedVertices.Add(edges[0].SelectedVetrexNumber);
-            double sum = edges[0].Weight;
-            edges.RemoveAt(0);
+            // Кажому элементу массива a[i] соответствует айдишник дерева, которому принадлежит вершина под номером i.
+            int[] treeIds = new int[verticesCount];
+            // Количество деревьев.
+            int treesCount = 0;
+            // Сумма рёбер.
+            double sum = 0;
 
-            while (edges.Count > 0)
+            for (int i = 0; i < edges.Count; i++)
             {
-                for(int i = 0; i < edges.Count; i++)
+                var edge = edges[i];
+
+                // Получаем айдишники деревьев, которым принадлежат вершины.
+                int firstVertexTreeId = treeIds[edge.NewVetrexNumber];
+                int secondVertexTreeId = treeIds[edge.SelectedVetrexNumber];
+
+                if (firstVertexTreeId == 0)
                 {
-                    var edge = edges[i];
-
-                    bool containsFirstVertex = false;
-                    bool containsSecondVertex = false;
-                    
-                    foreach (var addedVertex in addedVertices)
+                    if (secondVertexTreeId == 0)
                     {
-                        if (edge.NewVetrexNumber == addedVertex)
-                        {
-                            containsFirstVertex = true;
-
-                            if (containsSecondVertex)
-                            {
-                                break;
-                            }
-                        }
-                        if (edge.SelectedVetrexNumber == addedVertex)
-                        {
-                            containsSecondVertex = true;
-
-                            if (containsFirstVertex)
-                            {
-                                break;
-                            }
-                        }
-                    }
-
-                    if (containsFirstVertex)
-                    {
-                        if (containsSecondVertex)
-                        {
-                            edges.RemoveAt(i);
-                            i--;
-                        }
-                        else
-                        {
-                            addedVertices.Add(edge.SelectedVetrexNumber);
-                            sum += edge.Weight;
-                        }
+                        // Если обе вершины не принадлежат деревьям, создаём новое дерево.
+                        treesCount++;
+                        treeIds[edge.NewVetrexNumber] = treesCount;
+                        treeIds[edge.SelectedVetrexNumber] = treesCount;
                     }
                     else
                     {
-                        sum += edge.Weight;
-                        addedVertices.Add(edge.NewVetrexNumber);
+                        // Если одна из вершин принадлежит дереву, то просто добавляем вторую вершину к дереву.
+                        treeIds[edge.NewVetrexNumber] = treeIds[edge.SelectedVetrexNumber];
+                    }
 
-                        if (!containsSecondVertex)
+                    sum += edge.Weight;
+                }
+                else
+                {
+                    if (secondVertexTreeId == 0)
+                    {
+                        // Если одна из вершин принадлежит дереву, то просто добавляем вторую вершину к дереву.
+                        treeIds[edge.SelectedVetrexNumber] = treeIds[edge.NewVetrexNumber];
+                        sum += edge.Weight;
+                    }
+                    else if (firstVertexTreeId != secondVertexTreeId)
+                    {
+                        // Если вершины принадлежат разным деревьям, объединяем деревья.
+                        for (int j = 0; j < treeIds.Length; j++)
                         {
-                            addedVertices.Add(edge.SelectedVetrexNumber);
+                            if (treeIds[j] == secondVertexTreeId)
+                            {
+                                treeIds[j] = firstVertexTreeId;
+                            }
                         }
+
+                        sum += edge.Weight;
                     }
                 }
             }
 
             return sum;
         }
+
+        static void KruskallAlhorithmOptimized()
+        {
+            string[] firstStr = Console.ReadLine().Split(' ');
+
+            var verticesCount = Convert.ToInt32(firstStr[0]);
+            var edgesCount = Convert.ToInt32(firstStr[1]);
+
+            List<Edge> edges = new List<Edge>();
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var nextStr = Console.ReadLine().Split(' ');
+
+                var firstVertexNumber = Convert.ToInt32(nextStr[0]) - 1;
+                var secondVertexNumber = Convert.ToInt32(nextStr[1]) - 1;
+                int edgeWeight = Convert.ToInt32(nextStr[2]);
+
+                edges.Add(new Edge()
+                {
+                    NewVetrexNumber = firstVertexNumber,
+                    SelectedVetrexNumber = secondVertexNumber,
+                    Weight = edgeWeight
+                });
+            }
+
+            // Кажому элементу массива a[i] соответствует номер родительской вершины, которому принадлежит вершина под номером i.
+            int[] parents = new int[verticesCount];
+            for (int i = 0; i < parents.Length; i++)
+            {
+                parents[i] = i;
+            }
+            // Сумма рёбер.
+            double sum = 0;
+
+            for (int i = 0; i < edges.Count; i++)
+            {
+                var edge = edges[i];
+
+                // Получаем айдишники деревьев, которым принадлежат вершины.
+                int firstVertexTreeId = parents[edge.NewVetrexNumber];
+                int secondVertexTreeId = parents[edge.SelectedVetrexNumber];
+
+                firstVertexTreeId = FindSet(parents, firstVertexTreeId);
+                secondVertexTreeId = FindSet(parents, secondVertexTreeId);
+
+
+                if (firstVertexTreeId != secondVertexTreeId)
+                {
+                    // Если вершины принадлежат разным деревьям, объединяем деревья.
+                    parents[firstVertexTreeId] = secondVertexTreeId;
+
+                    sum += edge.Weight;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        public static int FindSet(int[] parents, int v)
+        {
+            if (parents[v] == v)
+                return v;
+            return parents[v] = FindSet(parents, parents[v]);
+        }
     }
 
-    struct Edge
+    struct Edge : IComparable
     {
         public int SelectedVetrexNumber { set; get; }
         public int NewVetrexNumber { set; get; }
-        public double Weight { set; get; }
+        public int Weight { set; get; }
+
+        public int CompareTo(object edge)
+        {
+            if (this.Weight > ((Edge)edge).Weight)
+                return 1;
+            else if (this.Weight == ((Edge)edge).Weight)
+            {
+                return this.NewVetrexNumber - ((Edge)edge).NewVetrexNumber;
+            }
+            else
+                return -1;
+        }
     }
 
-//    7 11
-//1 2 7
-//1 4 5
-//2 3 8
-//2 4 9
-//2 5 7
-//3 5 5
-//4 5 15
-//4 6 6
-//5 6 8
-//5 7 9
-//6 7 11
+    /*
+7 11
+1 2 7
+1 4 5
+2 3 8
+2 4 9
+2 5 7
+3 5 5
+4 5 15
+4 6 6
+5 6 8
+5 7 9
+6 7 11
+*/
 }
